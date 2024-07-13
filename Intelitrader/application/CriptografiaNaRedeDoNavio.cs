39b7da15e5846b30504ec3b2dc432cf4e6c7938f
filipeabmoura <filipemouradev@gmail.com>
2024-07-13 11:06:40 -0300

    feat: mensagem desencriptada

--- Intelitrader/application/CriptografiaNaRedeDoNavio.cs
+++ Intelitrader/application/CriptografiaNaRedeDoNavio.cs
@@ -17,7 +17,10 @@ Existe mais de uma maneira de resolver este problema, mas aqui está alguns conc
 Você saberá se conseguiu descriptografar a mensagem se ela for legível e em inglês.
 Pois, qualquer bit errado, você terá uma mensagem cheia de símbolos.
  
-            MINHA SOLUÇÃO: INVERTER BITS 7 E 8, INVERTER SEQUENCIA DE BITS 1234 POR 5678 
+            MINHA SOLUÇÃO: - PARA CADA SEQUÊNCIA DE 8 BITS: INVERTER BITS 7 E 8, INVERTER SEQUENCIA DE BITS 1234 POR 5678
+                           - PASSAR CADA CODIGO BINARIO DE 8 BITS PARA UMA FUNCAO QUE CONVERTE EM BASE DECIMAL
+                             E RETORNA O CHAR CORRESPONDENTE NA TABELA ASCII
+                           
  */
 
 namespace Intelitrader.application
@@ -26,15 +29,27 @@ namespace Intelitrader.application
     {
         private String mensagemCriptografada = "10010110 11110111 01010110 00000001 00010111 00100110 01010111 00000001 00010111 01110110 01010111 00110110 11110111 11010111 01010111 00000011";
 
+        private Char converteBinarioParaDecimal(String numBinarioOitoBits)
+        {
+            double numDecimal = 0;
+            int multiplicador = 1;
+            for (int i = numBinarioOitoBits.Length - 1; i >= 0; i--)
+            {
+                numDecimal += char.GetNumericValue(numBinarioOitoBits[i]) * multiplicador;
+                multiplicador *= 2; //1, 2, 4, 8, 16, 32, 64, 128
+            }
+            return Convert.ToChar(Convert.ToInt32((numDecimal))); // Double -> Int -> Char (Double -> Char é invalido)
+        }
+
         public void decifrar()
         {
             String janelaDeslizante = "";
             String mensagemDecifradaBinario = "";
             for(int i = 0; i <= mensagemCriptografada.Length - 8; i = i + 9)
             {
-                janelaDeslizante = mensagemCriptografada[i..(i+8)]; //index 0, 1, 2, ..., 8
+                janelaDeslizante = mensagemCriptografada[i..(i+8)]; //index 0, 1, 2, ..., 7 
                 
-                //inicio troca de posição dos primeiros 4 bista pelos últimos 4 bits da sequência
+                //inicio troca de posição dos primeiros 4 bits pelos últimos 4 bits da sequência
                 mensagemDecifradaBinario += janelaDeslizante[4..6]; //index 4, 5
                 
                 // fazendo not no penúltimo bit da sequência
@@ -54,11 +69,16 @@ namespace Intelitrader.application
                 //adicionando espaço em branco
                 mensagemDecifradaBinario += " ";
             }
-            
-            // nesse ponto, mensagemDecifradaBinario contém os 16 códigos ASCII (em binário de 8 bits) 
-            Console.WriteLine(mensagemDecifradaBinario);
 
-            //falta converter cada código no caractere correspondente
+            String mensagemDecifradaFinal = "";
+
+            for (int i = 0; i <= mensagemCriptografada.Length - 8; i = i + 9)
+            {   //chama funcao que recebe codigo ascii binario 8 bits (string) e retorna o char correspondete
+                mensagemDecifradaFinal += converteBinarioParaDecimal(mensagemDecifradaBinario[i..(i + 8)]);
+            }
+            
+            //mensagem final é impressa
+            Console.WriteLine(mensagemDecifradaFinal);
         }
     }
 }
